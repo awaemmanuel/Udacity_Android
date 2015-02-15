c582a7cf666a21b18df4a808b9d25e8f529b9d1b
Emmanuel Awa <eawa@brandeis.edu>
2015-02-15 14:20:50 -0500

    Refresh button added.. Uses permission to connect to internet gotten, and json response retrieved. Json not yet applied on list view.

--- app/src/main/java/com/example/bigsleek/sunshine/app/ForecastFragment.java
+++ app/src/main/java/com/example/bigsleek/sunshine/app/ForecastFragment.java
@@ -5,6 +5,9 @@ import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
@@ -32,10 +35,37 @@ public class ForecastFragment extends Fragment {
     public ForecastFragment() {
     }
 
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // Allow the fragment handle action bar stuff
+        setHasOptionsMenu(true);
+    }
+
+    // Inflate our forecastfragment xml unto the options menu
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        inflater.inflate(R.menu.forecastfragment, menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        if (id == R.id.action_refresh) {
+            FetchWeatherTask fetchWeatherTask = new FetchWeatherTask();
+            fetchWeatherTask.execute();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+
 
         // Forecast Array, that will be turned into an array List of strings
         String [] forecastArray = new String[]{
@@ -63,6 +93,8 @@ public class ForecastFragment extends Fragment {
                         R.layout.list_item_forecast, // Listview layout
                         R.id.list_item_forecast_TextView, // Listview textviews to populate
                         weekForecast);
+
+        View rootView = inflater.inflate(R.layout.fragment_main, container, false);
 
         // Get a reference to the ListView, and attach an adapter to this
         ListView listView;
@@ -113,6 +145,9 @@ public class ForecastFragment extends Fragment {
                 }
 
                 forecastJsonStr = buffer.toString();
+
+                // Verbose log statement to verify the async task returns something.
+                Log.v(LOG_TAG, "Forecast json string: " + forecastJsonStr);
 
             } catch (IOException e) {
                 Log.e(LOG_TAG, "Error ", e);
--- app/src/main/java/com/example/bigsleek/sunshine/app/MainActivity.java
+++ app/src/main/java/com/example/bigsleek/sunshine/app/MainActivity.java
@@ -41,4 +41,5 @@ public class MainActivity extends ActionBarActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
 }

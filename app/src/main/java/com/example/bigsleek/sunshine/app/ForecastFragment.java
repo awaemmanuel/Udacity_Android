4f3a0a7ce65396aa677bc13cabb695d80c138166
Emmanuel AWA <emmanuel.awa@amadeus.com>
2015-03-02 16:27:57 -0500

    Add the temperature list preference...

--- app/src/main/java/com/example/bigsleek/sunshine/app/ForecastFragment.java
+++ app/src/main/java/com/example/bigsleek/sunshine/app/ForecastFragment.java
@@ -66,13 +66,7 @@ public class ForecastFragment extends Fragment {
         int id = item.getItemId();
 
         if (id == R.id.action_refresh) {
-            FetchWeatherTask fetchWeatherTask = new FetchWeatherTask();
-
-            // Create a reference to a sharedpreference and retrieve location preference
-            SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(getActivity());
-            String location = preferences.getString(getString(R.string.pref_location_key),
-                    getString(R.string.pref_location_default));
-            fetchWeatherTask.execute(location);
+            updateWeather();
             return true;
         }
         return super.onOptionsItemSelected(item);
@@ -81,21 +75,6 @@ public class ForecastFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-
-
-        // Forecast Array, that will be turned into an array List of strings
-        String [] forecastArray = new String[]{
-                "Today - Sunny - 80 / 63",
-                "Tomorrow - Cloudy - 45 / 43",
-                "Weds - Sunny - 78 / 63",
-                "Thurs - Sunny - 85 / 63",
-                "Fri - Sunny - 22 / 11",
-                "Sat - Sunny - 34 / 34",
-                "Sun - Snow  - 44 / 67"
-        };
-
-        List<String> weekForecast = new ArrayList<String>(
-                Arrays.asList(forecastArray) );
 
             /*
              *  Now that we have some dummy data forecast,
@@ -108,7 +87,7 @@ public class ForecastFragment extends Fragment {
                         getActivity(), // Get application current context
                         R.layout.list_item_forecast, // Listview layout
                         R.id.list_item_forecast_TextView, // Listview textviews to populate
-                        weekForecast);
+                        new ArrayList<String>());
 
         View rootView = inflater.inflate(R.layout.fragment_main, container, false);
 
@@ -135,6 +114,27 @@ public class ForecastFragment extends Fragment {
         });
 
         return rootView;
+    }
+
+    // Private class that fetches weather data via an Async tax and populate
+    // list view
+    private void updateWeather() {
+        FetchWeatherTask fetchWeatherTask = new FetchWeatherTask();
+
+        // Create a reference to a sharedpreference and retrieve location preference
+        SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(getActivity());
+        String location = preferences.getString(getString(R.string.pref_location_key),
+                getString(R.string.pref_location_default));
+        fetchWeatherTask.execute(location);
+    }
+
+    /**
+     * Override onstart method with update weather as well.
+     */
+    @Override
+    public void onStart() {
+        super.onStart();
+        updateWeather();
     }
 
     public class FetchWeatherTask extends AsyncTask<String, Void, String[]> {
@@ -254,7 +254,16 @@ public class ForecastFragment extends Fragment {
         /**
          * Prepare the weather high/lows for presentation.
          */
-        private String formatHighLows(double high, double low) {
+        private String formatHighLows(double high, double low, String unitType) {
+
+            if (unitType.equals(getString(R.string.pref_units_imperial))) {
+                high = (high * 1.8) + 32;
+                low = (low * 1.8) + 32;
+            }
+            else if (!unitType.equals(getString(R.string.pref_units_metric))) {
+                Log.d(LOG_TAG, "Unit type not found: " + unitType);
+            }
+
             // For presentation, assume the user doesn't care about tenths of a degree.
             long roundedHigh = Math.round(high);
             long roundedLow = Math.round(low);
@@ -301,6 +310,19 @@ public class ForecastFragment extends Fragment {
             dayTime = new Time();
 
             String[] resultStrs = new String[numDays];
+
+
+            // Data is fetched in Celsius by default.
+            // If user prefers to see in Fahrenheit, convert the values here.
+            // We do this rather than fetching in Fahrenheit so that the user can
+            // change this option without us having to re-fetch the data once
+            // we start storing the values in a database.
+
+            SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(getActivity());
+            String unitType = sharedPreferences.getString(getString(R.string.pref_units_key),
+                    getString(R.string.pref_units_metric));
+
+
             for(int i = 0; i < weatherArray.length(); i++) {
                 // For now, using the format "Day, description, hi/low"
                 String day;
@@ -328,7 +350,7 @@ public class ForecastFragment extends Fragment {
                 double high = temperatureObject.getDouble(OWM_MAX);
                 double low = temperatureObject.getDouble(OWM_MIN);
 
-                highAndLow = formatHighLows(high, low);
+                highAndLow = formatHighLows(high, low, unitType);
                 resultStrs[i] = day + " - " + description + " - " + highAndLow;
             }
 

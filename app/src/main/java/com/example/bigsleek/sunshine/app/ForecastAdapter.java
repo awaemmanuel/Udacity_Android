414d77db945fc8216c48c40d071bfd6d90645d7a
Emmanuel Awa <eawa@brandeis.edu>
2015-03-29 00:26:53 -0400

    Introduce View types.. Choose today or future date.

--- app/src/main/java/com/example/bigsleek/sunshine/app/ForecastAdapter.java
+++ app/src/main/java/com/example/bigsleek/sunshine/app/ForecastAdapter.java
@@ -9,27 +9,42 @@ import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-import com.example.bigsleek.sunshine.app.ForecastFragment;
-import com.example.bigsleek.sunshine.app.R;
-import com.example.bigsleek.sunshine.app.Utility;
-
 /**
  * {@link ForecastAdapter} exposes a list of weather forecasts
  * from a {@link android.database.Cursor} to a {@link android.widget.ListView}.
  */
 public class ForecastAdapter extends CursorAdapter {
+
+    private static final int VIEW_TYPE_TODAY = 0;
+    private static final int VIEW_TYPE_FUTURE_DAY = 1;
+    private static final int VIEW_TYPE_COUNT = 2;
+
     public ForecastAdapter(Context context, Cursor c, int flags) {
         super(context, c, flags);
     }
 
-    /*
-        Remember that these views are reused as needed.
+    @Override
+    public int getItemViewType(int position) {
+        return position == 0 ? VIEW_TYPE_TODAY : VIEW_TYPE_FUTURE_DAY;
+    }
+
+    @Override
+    public int getViewTypeCount() {
+        return VIEW_TYPE_COUNT;
+    }
+
+    /**
+     * Copy/paste note: Replace existing newView() method in ForecastAdapter with this one.
      */
     @Override
     public View newView(Context context, Cursor cursor, ViewGroup parent) {
-        View view = LayoutInflater.from(context).inflate(R.layout.list_item_forecast, parent, false);
+        // Choose the layout type
+        int viewType = getItemViewType(cursor.getPosition());
+        int layoutId = -1;
 
-        return view;
+        // TODO: Determine layoutId from viewType
+        layoutId = (viewType == VIEW_TYPE_TODAY ? R.layout.list_item_forecast_today : R.layout.list_item_forecast);
+        return LayoutInflater.from(context).inflate(layoutId, parent, false);
     }
 
     /*

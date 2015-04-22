4a49d80f7add39589f3ea1eadd397a0420f281e4
Emmanuel AWA <emmanuel.awa@amadeus.com>
2015-04-22 07:15:58 -0400

    Replace the Async Task with a service so all the heavy lifting from the weather api can be done in a service. This way even when the device is rotated it doesn't start the Async Task all over again.

--- app/src/main/java/com/example/bigsleek/sunshine/app/ForecastFragment.java
+++ app/src/main/java/com/example/bigsleek/sunshine/app/ForecastFragment.java
@@ -15,6 +15,7 @@
  */
 package com.example.bigsleek.sunshine.app;
 
+import android.content.Intent;
 import android.database.Cursor;
 import android.net.Uri;
 import android.os.Bundle;
@@ -32,6 +33,7 @@ import android.widget.AdapterView;
 import android.widget.ListView;
 
 import com.example.bigsleek.sunshine.app.data.WeatherContract;
+import com.example.bigsleek.sunshine.app.service.SunshineService;
 
 /**
  * Encapsulates fetching the forecast and displaying it as a {@link ListView} layout.
@@ -177,9 +179,10 @@ public class ForecastFragment extends Fragment implements LoaderManager.LoaderCa
     }
 
     private void updateWeather() {
-        FetchWeatherTask weatherTask = new FetchWeatherTask(getActivity());
-        String location = Utility.getPreferredLocation(getActivity());
-        weatherTask.execute(location);
+        Intent intent = new Intent(getActivity(), SunshineService.class);
+        intent.putExtra(SunshineService.LOCATION_QUERY_EXTRA,
+                Utility.getPreferredLocation(getActivity()));
+        getActivity().startService(intent);
     }
 
     @Override
